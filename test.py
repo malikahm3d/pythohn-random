d184e36bffc01d494c44129e82736a6cb6447a0a
malikahm3d <44858076+malikahm3d@users.noreply.github.com>
2021-06-05 14:23:50 +0300

    if else

--- test.py
+++ test.py
@@ -2,3 +2,14 @@ people = ['malk', 'not malik', 'somene else']
 numbers = [2, 45, 69]
 people.extend(numbers)
 print(people)
+
+isMale = False
+isTall = False
+if isMale and isTall:
+    print('tall male')
+elif isMale and not(isTall):
+    print("short male")
+elif not(isMale) and isTall:
+    print("tall female")
+else:
+    print("short female")
